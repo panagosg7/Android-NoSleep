2d2cb4112adae424768a9a417430827a6eb7e78b
John McCullough <jcmdev0@gmail.com>
2012-03-15 10:32:40 -0700

    Moved info.json to scratch.  WorkConsumer expanded.

--- edu.ucsd.salud.mcmutton/src/main/java/edu/ucsd/salud/mcmutton/WorkConsumer.java
+++ edu.ucsd.salud.mcmutton/src/main/java/edu/ucsd/salud/mcmutton/WorkConsumer.java
@@ -1,14 +1,24 @@
 package edu.ucsd.salud.mcmutton;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.util.NoSuchElementException;
 import java.util.Properties;
+import java.util.Set;
+
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
 
 import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.ZooKeeper;
+
+import edu.ucsd.salud.mcmutton.apk.Wala;
 
 public class WorkConsumer implements Watcher {
 	ZooKeeper mZookeeper;
@@ -43,6 +53,9 @@ public class WorkConsumer implements Watcher {
 			ApkCollection collection = new ApkCollection();
 			collection.setCaching(false);
 			
+			File resultsDir = new File(ApkInstance.sScratchPath + File.separator + "results");
+			resultsDir.mkdirs();
+			
 			while (true) {
 				try {
 					String work = (String)mWorkSet.getWork();
@@ -53,10 +66,74 @@ public class WorkConsumer implements Watcher {
 					}
 					
 					System.out.println(apk.getPath());
+					
 					apk.writeInfo();
 					if (apk.hasWakelockCalls()) {
 						apk.requiresOptimized();
 					}
+					
+					File result_target = new File(resultsDir + File.separator + work + ".json");
+					
+					JSONObject obj = new JSONObject();
+					obj.put("hasWakelockCalls", apk.hasWakelockCalls());
+					if (apk.hasWakelockCalls()) {
+						obj.put("successfullyOptimized", apk.successfullyOptimized());
+						obj.put("optimizationException", apk.getOptException());
+						Set<String> phantoms = apk.getOptPhantoms();
+						JSONArray phantom_array = new JSONArray();
+						phantom_array.addAll(phantoms);
+						obj.put("optimizationPhantoms", phantom_array);
+						
+						if (apk.successfullyOptimized()) {
+							JSONObject panos_result = new JSONObject();
+							try {
+								Set<String> colors = apk.panosAnalyze();
+								JSONArray colors_array = new JSONArray();
+								colors_array.addAll(colors);
+								panos_result.put("colors", colors);
+							} catch (Exception e) {
+								panos_result.put("_exception", e.toString());
+								ByteArrayOutputStream baos = new ByteArrayOutputStream();
+								e.printStackTrace(new PrintStream(baos));
+								panos_result.put("_stackTrace", baos.toString());
+								baos.close();
+							} catch (Error e) {
+								panos_result.put("_error", e.toString());
+								ByteArrayOutputStream baos = new ByteArrayOutputStream();
+								e.printStackTrace(new PrintStream(baos));
+								panos_result.put("_stackTrace", baos.toString());
+								baos.close();
+							}
+							obj.put("panosResult", panos_result);
+							
+							
+							JSONObject pattern_result = new JSONObject();
+							try {
+								Wala.UsageType usageType = Wala.UsageType.UNKNOWN;
+								usageType = apk.analyze();
+								pattern_result.put("usageType", usageType);
+							} catch (Exception e) {
+								pattern_result.put("_exception", e.toString());
+								ByteArrayOutputStream baos = new ByteArrayOutputStream();
+								e.printStackTrace(new PrintStream(baos));
+								pattern_result.put("_stackTrace", baos.toString());
+								baos.close();
+							} catch (Error e) {
+								pattern_result.put("_error", e.toString());
+								ByteArrayOutputStream baos = new ByteArrayOutputStream();
+								e.printStackTrace(new PrintStream(baos));
+								pattern_result.put("_stackTrace", baos.toString());
+								baos.close();
+							}
+							obj.put("patternResult", pattern_result);
+						}
+
+					}
+					
+					System.out.println("writing " + result_target);
+					FileWriter writer = new FileWriter(result_target);
+					obj.write(writer);
+					writer.close();
                 } catch (ApkException e) {
                     System.err.println("apk err: " + e.toString());
                     continue;
