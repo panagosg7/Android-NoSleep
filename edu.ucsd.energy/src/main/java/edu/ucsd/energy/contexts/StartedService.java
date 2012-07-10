0afe5f62a78449d9878e4e5049f4aa0bcfd32509
Panagiotis Vekris <pvekris@cs.ucsd.edu>
2012-07-09 17:20:51 -0700

    Committing a temp

---
+++ edu.ucsd.energy/src/main/java/edu/ucsd/energy/contexts/StartedService.java
@@ -0,0 +1,14 @@
+package edu.ucsd.energy.contexts;
+
+import com.ibm.wala.classLoader.IClass;
+
+import edu.ucsd.energy.managers.GlobalManager;
+
+public class StartedService extends Service {
+
+	public StartedService(GlobalManager gm, IClass c) {
+		super(gm, c);
+		// TODO Auto-generated constructor stub
+	}
+
+}
